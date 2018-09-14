9e34ce8059053905cb18ae803d494d2b2c015c1c
Anton Arhipov <ant.arhipov@gmail.com>
2018-09-14 14:36:10 +0300

    first commit

---
+++ src/main/java/org/arhan/Component.java
@@ -0,0 +1,5 @@
+package org.arhan;
+
+public class Component {
+
+}
---
+++ src/test/java/org/arhan/AppTest.java
@@ -0,0 +1,13 @@
+package org.arhan;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
